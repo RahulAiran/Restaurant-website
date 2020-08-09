fbc30e85e28719fc771a1bd64951cec335131ada
Rahul Airan <airanrahul1999@gmail.com>
2020-08-09 21:19:03 +0530

    Functional Components

--- src/components/MenuComponent.js
+++ src/components/MenuComponent.js
@@ -1,30 +1,27 @@
-import React , { Component } from 'react';
+import React  from 'react';
 import { Card, CardImg , CardImgOverlay , CardText , CardBody , CardTitle } from 'reactstrap';
 
 
-class Menu extends Component {
-
-	constructor(props){
-		super(props);
-
-	}
-
-
-
-	render()
-	{
-
-		const menu = this.props.dishes.map((dish) =>{
-			return (
-					<div key={dish.id} className="col-12 col-md-5 m-1">
+function RenderMenuItem({dish, onClick}){
+	return(
 						<Card key={dish.id}
-                        onClick={() => this.props.onClick(dish.id)}>
+                        onClick={() => onClick(dish.id)}>
 								<CardImg width="100%" src={dish.image} alt={dish.name} />
 							<CardImgOverlay>
 							 <CardTitle>{dish.name}</CardTitle>
 						
 							 </CardImgOverlay>
 						</Card>
+		);
+
+}
+
+const Menu=(props) => {
+
+	const menu = props.dishes.map((dish) =>{
+			return (
+					<div key={dish.id} className="col-12 col-md-5 m-1">
+					<RenderMenuItem dish={dish} onClick={props.onClick} />
 					</div>
 				);
 		});
@@ -37,7 +34,7 @@ class Menu extends Component {
 					</div>
 				</div>
 			);
-	}
 }
+		
 
 export default Menu;
